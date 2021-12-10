20ad2c99552119fee3724173b62dddaecab61332
frolovdev <frolovandrey752@gmail.com>
2021-12-10 23:01:01 +0300

    delete unused code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,4 @@
 export async function handleRequest(request: Request, env: Bindings) {
-  // Match route against pattern /:name/*action
-  const url = new URL(request.url);
-  const match = /\/(?<name>[^/]+)(?<action>.*)/.exec(url.pathname);
-  if (!match?.groups) {
-    // If we didn't specify a name, default to "test"
-    return Response.redirect(`${url.origin}/test/increment`, 302);
-  }
-
-  // Forward the request to the named Durable Object...
-  // const { COUNTER } = env;
-  // const id = COUNTER.idFromName(match.groups.name);
-  // const stub = COUNTER.get(id);
-  // // ...removing the name prefix from URL
-  // url.pathname = match.groups.action;
-
   const json = JSON.stringify({}, null, 2);
   return new Response(json, {
     status: 200,

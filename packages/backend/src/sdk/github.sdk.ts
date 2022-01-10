ac7b511f6aeee0503878c671f1c33bb43b7b21b0
ippsav <69125922+ippsav@users.noreply.github.com>
2022-01-10 16:38:15 +0100

    fix (#14)

--- packages/backend/src/sdk/github.sdk.ts
+++ packages/backend/src/sdk/github.sdk.ts
@@ -1,9 +1,11 @@
 export const callGithub = async (path: string, fetchOptions: RequestInit): Promise<Response> => {
-  const response = await fetch(`${path}`, {
+  const headers = {
+    ...fetchOptions.headers,
+    'User-Agent': 'bestyjs',
+  };
+  const response = await fetch(path, {
     ...fetchOptions,
-    headers: {
-      'User-Agent': 'bestyjs',
-    },
+    headers,
   });
   return response;
 };

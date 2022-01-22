35e2af6106ebc8b0023202b52552cc90ef4cbc46
Andrey Frolov <30667180+frolovdev@users.noreply.github.com>
2022-01-22 16:29:56 +0300

    migrate to sentry (#18)

--- packages/backend/src/lib/logger.ts
+++ packages/backend/src/lib/logger.ts
@@ -1,29 +1,45 @@
-import Rollbar from 'rollbar';
+import Toucan from 'toucan-js';
 import { Logger } from '../types';
 
 type Data = any;
 
 export const error =
-  (env: Bindings, rollbar: Rollbar) =>
+  (env: Bindings, sentry: Toucan) =>
   (e: Error, options: { message?: string; data?: Data } = {}) => {
     if (env.ENVIRONMENT === 'development') {
       console.error(e, options);
       return;
     }
 
-    return rollbar.error(e, options);
+    sentry.withScope((scope) => {
+      scope.setExtra('meta', options);
+      console.log('qwpkoEPJOibqwoeopQW');
+      sentry.captureException(e);
+    });
   };
 
-export const debug = (env: Bindings, rollbar: Rollbar) => (message: string, data?: Data) => {
+export const debug = (env: Bindings, sentry: Toucan) => (message: string, data?: Data) => {
   if (env.ENVIRONMENT === 'development') {
     console.log(message, data);
     return;
   }
 
-  return rollbar.debug(message, data);
+  sentry.withScope((scope) => {
+    scope.setExtra('meta', data);
+    sentry.captureMessage(message);
+  });
 };
 
-export function createLogger(env: Bindings): Logger {
-  const rollbar = new Rollbar({ accessToken: env.ROLLBAR_TOKEN });
-  return { error: error(env, rollbar), debug: debug(env, rollbar) };
+export function createLogger(request: Request, env: Bindings, context: ExecutionContext): Logger {
+  const sentry = new Toucan({
+    dsn: env.SENTRY_DSN,
+    context,
+    request,
+    allowedHeaders: ['user-agent'],
+    allowedSearchParams: /(.*)/,
+    rewriteFrames: {
+      root: '/',
+    },
+  });
+  return { error: error(env, sentry), debug: debug(env, sentry) };
 }
